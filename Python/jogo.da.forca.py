84a3c34e8ae94b5428a5e3598409eafc0da8b638
Gustavo Bento <gugu.lima21@hotmail.com>
2021-12-11 09:36:06 -0300

    Importação inicial

--- Python/jogo.da.forca.py
+++ Python/jogo.da.forca.py
@@ -11,4 +11,18 @@ digitadas = []
 for letra in palavra_secreta:
     digitadas.append('__')
 
+def mostrar_digitadas():
+    print('Palavra secreta: ', end='')
+    for item in digitadas:
+        print(f'{item} ', end='')
+
+def salvar_letra (letra):
+    ind = 0
+    encontrou = 0
+    for c in palavra_secreta:
+        if (c == letra):
+            digitadas[ind] = letra
+            encontrou += 1
+        ind += 1
+    return encontrou
     
